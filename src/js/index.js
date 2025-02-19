de94e1e82148b9954a79247cfd9d9f9df77079fb
Rodrigo Camilo <digo-camilo2@hotmail.com>
2025-02-19 16:53:10 -0300

    card movement and final adjustments

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,24 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+cartoes.forEach((cartao) => {
+    cartao.addEventListener("click", function(){
+        const cartaVirada = cartao.querySelector(".carta-virada");
+
+        cartao.classList.toggle("virar");
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+    });
+});
+
 function avancarCartao() {
     if (cartaoAtual === cartoes.length - 1) {
         return;
     }
 
-    const cartaoSelecionado = document.querySelector(".selecionado")
+    const cartaoSelecionado = document.querySelector(".selecionado");
 
     cartaoAtual++;
     cartoes[cartaoAtual].classList.add("selecionado");
